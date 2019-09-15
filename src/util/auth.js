020d08ab550742b119f323a4637b91213534ec46
Vamshi Krishna Pothuka <pvamshi@gmail.com>
2019-09-15 23:43:06 +0530

    CircleCI config

--- src/util/auth.js
+++ src/util/auth.js
@@ -13,26 +13,27 @@ export async function getUser() {
       window.location.href.match(/\?code=(.*)/) &&
       window.location.href.match(/\?code=(.*)/)[1];
 
-    if (code) {
-      try {
-        const response = await fetch(
-          "https://pqsq24y7vb.execute-api.us-east-1.amazonaws.com/github/authenticate?code=" + code
-        );
-        const { access_token: token } = await response.json();
-        const response2 = await fetch("https://api.github.com/user", {
-          headers: { Authorization: `token ${token}` }
-        });
-        if (response2.status > 400) {
-          throw new Error("Unauthorized");
-        }
-        const user = Object.assign(await response2.json(), { token });
-        localStorage.setItem(GITHUB_USER, JSON.stringify(Object.assign(user)));
-
-        return user;
-      } catch (e) {
-        return null;
-      }
+    if (!code) {
+      return null;
     }
-    return null;
+    try {
+      const response = await fetch(
+        "https://pqsq24y7vb.execute-api.us-east-1.amazonaws.com/github/authenticate?code=" +
+          code
+      );
+      const { access_token: token } = await response.json();
+      const response2 = await fetch("https://api.github.com/user", {
+        headers: { Authorization: `token ${token}` }
+      });
+      if (response2.status > 400) {
+        throw new Error("Unauthorized");
+      }
+      const user = Object.assign(await response2.json(), { token });
+      localStorage.setItem(GITHUB_USER, JSON.stringify(Object.assign(user)));
+
+      return user;
+    } catch (e) {
+      return null;
+    }
   }
 }
