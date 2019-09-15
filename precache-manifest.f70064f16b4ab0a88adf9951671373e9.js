1503f8fc4aff74e355eb80fc8d09e663048d3370
Vamshi Krishna Pothuka <pvamshi@gmail.com>
2019-09-15 22:57:22 +0530

    Updates

---
+++ precache-manifest.f70064f16b4ab0a88adf9951671373e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3bdaf8dc4dc520adb4fcc8b01232c8b",
+    "url": "/runjs/index.html"
+  },
+  {
+    "revision": "63679ad70f0e325eee5d",
+    "url": "/runjs/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "4e8f42708a9dc1437819",
+    "url": "/runjs/static/js/2.6693431e.chunk.js"
+  },
+  {
+    "revision": "63679ad70f0e325eee5d",
+    "url": "/runjs/static/js/main.c7e7f52a.chunk.js"
+  },
+  {
+    "revision": "0cf11281cbac52486de1",
+    "url": "/runjs/static/js/runtime~main.95a452ee.js"
+  }
+]);
